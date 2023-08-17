8cf5f716f88e57b3097187db0412163ec1a04696
Gregor Vilkner <gregor.vilkner@thinkiq.com>
2023-08-17 12:09:30 -0400

    added keyvault and servicebus samples.

---
+++ HelloKeyvault/Program.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.Azure.KeyVault;
+using Microsoft.Identity.Client;
+using System;
+using System.Linq;
+
+namespace HelloKeyvault
+{
+    class Program
+    {
+        static IConfidentialClientApplication app;
+        static async System.Threading.Tasks.Task Main(string[] args)
+        {
+            // mdesk
+            string tenantId = "14c2fac0-aa1f-4bd3-beb8-9e58dfb9b18a";
+            string ClientSecret = "~938Q~maRFLoarelt83lumyEnSL4CV4TaHPf~bhZ";
+            string ClientId = "fbc3619f-d3b9-4fef-85a2-a8599029a232";
+            string authority = $"https://login.microsoftonline.com/{tenantId}/oauth2v2.0/token";
+
+            app = ConfidentialClientApplicationBuilder.Create(ClientId)
+                .WithAuthority(authority)
+                .WithClientSecret(ClientSecret)
+                .Build();
+
+            KeyVaultClient client = new KeyVaultClient(async (authority, resource, scope) =>
+            {
+                string[] scopes = new string[] { "https://vault.azure.net/.default" };
+                AuthenticationResult authenticationResult = await app.AcquireTokenForClient(scopes)
+                    .ExecuteAsync();
+
+                return authenticationResult.AccessToken;
+
+            });
+
+
+            string keyVaultName = "MarconiKeyVaultClient";
+            var kvUri = "https://" + keyVaultName + ".vault.azure.net";
+            var newse = await client.SetSecretAsync(kvUri, "newSecret", Guid.NewGuid().ToString());
+            var secret = (await client.GetSecretsAsync(kvUri)).First();
+            var v = await client.GetSecretAsync(secret.Identifier.Identifier);
+            var v2 = await client.GetSecretVersionsAsync(kvUri, secret.Identifier.Name);
+            foreach(var aSecret in v2)
+            {
+                Console.WriteLine($"{aSecret.Identifier.Version} - {aSecret.Attributes.Created} - {aSecret.Attributes.Enabled} - {aSecret.Attributes.Updated} ");
+            }
+        }
+
+
+    }
+}
---
+++ HelloServiceBus/Program.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Azure.Management.Fluent;
+using Microsoft.Azure.Management.ServiceBus.Fluent;
+using Microsoft.Azure.ServiceBus.Management;
+using System;
+using System.Linq;
+
+namespace HelloServiceBus
+{
+    class Program
+    {
+        static async System.Threading.Tasks.Task Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            string serviceBusConnectionString = "Endpoint=sb://marconirelay.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=0Sj+R0v7xdUiqVPavaVymH0igrrsq9gd7+ASbE7pTn4=";
+
+            var managementClient = new ManagementClient(serviceBusConnectionString);
+
+            var allQueues = await managementClient.GetQueuesAsync();
+
+            if(allQueues.Where(x=>x.Path=="testqueue").Count() > 0)
+            {
+                // delete the existing test queue
+                await managementClient.DeleteQueueAsync("testqueue");
+            }
+
+            var newQueue = await managementClient.CreateQueueAsync("testqueue");
+
+            allQueues = await managementClient.GetQueuesAsync();
+
+
+
+        }
+    }
+}
